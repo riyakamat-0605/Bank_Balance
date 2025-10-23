49bc782e68c858de5efe4463dd77556687c631ed
riyakamat-0605 <riyakamat2006@gmail.com>
2025-10-23 14:49:29 +0530

    save

---
+++ Balance_check.py
@@ -0,0 +1,8 @@
+initial_balance = float(input("Enter initial balance: ₹"))
+deposit = float(input("Enter deposit amount: ₹"))
+
+new_balance = initial_balance + deposit
+
+print(f"Initial Balance: ₹{initial_balance}")
+print(f"Deposit: ₹{deposit}")
+print(f"New Balance after deposit: ₹{new_balance}")
