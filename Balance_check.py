1c9e11e507f5bab2448888d5960b293b6c692b81
riyakamat-0605 <riyakamat2006@gmail.com>
2025-10-23 14:52:30 +0530

    save to withdrawal-feature

--- Balance_check.py
+++ Balance_check.py
@@ -1,8 +1,16 @@
 initial_balance = float(input("Enter initial balance: ₹"))
 deposit = float(input("Enter deposit amount: ₹"))
 
-new_balance = initial_balance + deposit
-
+balance = initial_balance + deposit
 print(f"Initial Balance: ₹{initial_balance}")
 print(f"Deposit: ₹{deposit}")
-print(f"New Balance after deposit: ₹{new_balance}")
+print(f"New Balance after deposit: ₹{balance}")
+
+withdraw = float(input("Enter amount to withdraw: ₹"))
+
+if withdraw > balance:
+    print("Insufficient balance!")
+else:
+    balance -= withdraw
+    print(f"Withdraw: ₹{withdraw}")
+    print(f"Final Balance: ₹{balance}")
